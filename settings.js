b1b507d18980456ee3f064d1a23c88cdcaf11265
katanyakawan <117534411+katanyakawan@users.noreply.github.com>
2022-11-06 21:12:26 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,10 +10,10 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 global.hituet = 0
-global.gopayno = "6281775172693"
-global.danano = "6281775172693"
-global.shopeepayno = "6281775172693"
-global.creator = "6281775172693@s.whatsapp.net"
+global.gopayno = "6285877346651"
+global.danano = "6285877346651"
+global.shopeepayno = "6285877346651"
+global.creator = "6285877346651@s.whatsapp.net"
 global.thumb = fs.readFileSync(`./image/thumb.png`)
 global.qrisdonate = fs.readFileSync(`./image/qris.jpg`)
 global.fake = `📍Zenss Bot WhatsApp`
@@ -23,9 +23,9 @@ global.antilink = false
 global.antiwame = false
 global.autodltt = false
 global.autosticker = false
-global.ownerNomor = '6281775172693'
+global.ownerNomor = '6285877346651'
 global.ownerName = 'Creator Alimran'
-global.ownerNumber = ["6281775172693@s.whatsapp.net"]
+global.ownerNumber = ["6285877346651@s.whatsapp.net"]
 global.cek1 = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
 global.mess = {
     wait: 'Wait Sis Please be patient',
